0830d66b793bbbe34322522ca9305416fe2f45ea
Iten Elhak <56697800+Iten-No-404@users.noreply.github.com>
2021-12-21 15:24:57 +0200

    Connecting Fetch and Decode

--- Integration.vhd
+++ Integration.vhd
@@ -7,14 +7,20 @@ ENTITY integration IS
 GENERIC ( n : integer :=16 ); -- For register size
 PORT (
     clk,rst: IN std_logic;
-    in_signal: IN std_logic;
-    out_signal: IN std_logic;
     in_port: IN std_logic_vector(n-1 DOWNTO 0);
     out_port: OUT std_logic_vector(n-1 DOWNTO 0)
 );
 END integration;
 
 ARCHITECTURE arch1 OF integration IS
+
+    COMPONENT control_unit_VHDL is
+        port (
+        opcode: in std_logic_vector(4 downto 0);
+        reset: in std_logic;
+        RegWrite,WB_To_Reg,HLT,SETC,RST,OUT_PORT_SIG,IN_PORT_SIG: out std_logic
+        );
+    end COMPONENT;
 
     COMPONENT fetch IS
     GENERIC ( 
@@ -86,10 +92,40 @@ ARCHITECTURE arch1 OF integration IS
          );
     end COMPONENT;
         
+    SIGNAL Instruction,NewPc:  std_logic_vector(31 DOWNTO 0);
+    -- SIGNAL InstructionOut,NewPcOut:  std_logic_vector(31 DOWNTO 0);
+    SIGNAL RegWrite,WB_To_Reg,HLT,SETC,RST,OUT_PORT_SIG,IN_PORT_SIG: std_logic;
+    SIGNAL alwayson: std_logic := '1';
+    SIGNAL PCSrc: std_logic_vector(1 DOWNTO 0);
+    SIGNAL pc_01, pc_10: std_logic_vector(31 DOWNTO 0);  --> Need to connect this
+    SIGNAL ifidin,ifidout: std_logic_vector(127 DOWNTO 0);
+    SIGNAL opcode: std_logic_vector(4 downto 0);
+    SIGNAL pc_outD: std_logic_vector(31 DOWNTO 0); --> Need to connect this
+    SIGNAL rsrc1addrD, rsrc2addrD,rdstaddrD: std_logic_vector(2 DOWNTO 0); --> Need to connect this
+    SIGNAL extrabits: std_logic_vector(1 DOWNTO 0); --> Need to connect this
+    SIGNAL immmediate_offsetD: std_logic_vector(15 DOWNTO 0); --> Need to connect this
+    SIGNAL in_dataD: std_logic_vector(n-1 DOWNTO 0); --> Need to connect this to id/ex buffer as IN
+    SIGNAL write_reg: std_logic_vector(2 DOWNTO 0); --> Need to connect this
+    SIGNAL write_data: std_logic_vector(n-1 DOWNTO 0); --> Need to connect this
+    SIGNAL read_data_1D, read_data_2D, read_data_3D: std_logic_vector(n-1 DOWNTO 0); --> Need to connect this to id/ex buffer as IN
+    SIGNAL ccr_inD, ccr_outD: std_logic_vector(3 DOWNTO 0); --> Need to connect this
+    SIGNAL sp_inD, sp_outD: std_logic_vector(31 DOWNTO 0);--> Need to connect this
+    SIGNAL int_signal, rti_signal: std_logic; --> Need to connect this
 
     BEGIN
 
-    
+        cu: control_unit_VHDL PORT MAP(opcode, rst, RegWrite, WB_To_Reg, HLT, SETC, RST, OUT_PORT_SIG, IN_PORT_SIG);
+
+        fetchs: fetch PORT MAP(HLT, clk, rst, alwayson, PCSrc, pc_01, pc_10, NewPc,Instruction);
+
+        ifid: generic_buffer GENERIC MAP(64) PORT MAP(ifidin, ifidout, clk, rst);
+
+        ds: decode_stage GENERIC MAP (n) PORT MAP(ifidout(63 DOWNTO 32),ifidout(31 DOWNTO 0), pc_outD, opcode,
+        rsrc1addrD, rsrc2addrD,rdstaddrD, extrabits, immmediate_offsetD, clk, rst, RegWrite, IN_PORT_SIG, OUT_PORT_SIG, in_port, in_dataD, out_port,
+        write_reg, write_data, read_data_1D, read_data_2D, read_data_3D, ccr_inD, ccr_outD, sp_inD, sp_outD, int_signal, rti_signal);
+
+
+        ifidin <= Intruction & NewPc;
 
 
 END arch1;

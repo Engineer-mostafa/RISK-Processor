965e677eba0c14a643a2e9a17d287365de5901b5
reem <reem.atalah1@gmail.com>
2022-01-08 20:03:00 +0200

    memory full implementation, not tested yet

--- Data Memory.vhd
+++ Data Memory.vhd
@@ -4,12 +4,14 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.all;
 
 ENTITY Dmemory IS
+GENERIC ( 
+	n : integer :=2
+	); 
 	PORT(
         -- MemRead, MemWrite,
         Clk,Rst,enable : IN std_logic;
         OP : IN std_logic_vector(4 DOWNTO 0);
-		-- Address : IN  std_logic_vector(19 DOWNTO 0);
-		Write_data : IN std_logic_vector(15 DOWNTO 0);
+		Write_data,Address,Rscr2 : IN std_logic_vector(15 DOWNTO 0);
 		Read_data : OUT  std_logic_vector(15 DOWNTO 0);
         empty_stack	: OUT std_logic
         );
@@ -27,17 +29,17 @@ ARCHITECTURE a_Dmemory OF Dmemory IS
 
 	TYPE Dmemory_type IS ARRAY(0 TO (2**20)-1) OF std_logic_vector(15 DOWNTO 0);
 	SIGNAL data_memory : Dmemory_type;
-    signal empty,one,ze :  std_logic_vector(31 downto 0);
+    -- signal empty,one :  std_logic_vector(31 downto 0);
     signal out_SP, out_SP_in,reset_val : std_logic_vector(31 DOWNTO 0);
 
 	
 	BEGIN 
 
         -- begin with reset --> then enable
-        empty <= (OTHERS=>'0');
-        one <= (OTHERS=>'1');
-        ze <= (OTHERS=>'U');
-        reset_val <= empty(11 downto 0) & one(19 downto 0);
+        -- empty <= (OTHERS=>'0');
+        -- one <= (OTHERS=>'1');
+        
+        reset_val <= "00000000000100000000000000000000";
         SP: dff port map(Clk,Rst,enable,reset_val,out_SP_in,out_SP);
 
         process(Clk)
@@ -45,26 +47,42 @@ ARCHITECTURE a_Dmemory OF Dmemory IS
         IF(rising_edge(Clk)) THEN
             -- push
             IF(OP = "01100")THEN 
-                --put in memory Rdst value
-                data_memory(to_integer(unsigned(out_SP_in(19 downto 0)))) <= Write_data; 
                 --dec SP
                 out_SP_in <= std_logic_vector( unsigned(out_SP_in) -1); 
+
+                --put in memory Rdst value
+                data_memory(to_integer(unsigned(out_SP_in(19 downto 0)))) <= Write_data; 
                 empty_stack <= '0';
 
             --pop
             elsif (OP = "01101") then 
                 --check empty stack
-                if (out_SP = ze) then
+                if (out_SP = reset_val) then
                     empty_stack <= '1';
                 else
                     --update read data(R[Rdst])
-                    Read_data <= data_memory(to_integer(unsigned(out_SP_in(19 downto 0))));
+                    Read_data <= data_memory(to_integer(unsigned(out_SP(19 downto 0))));
                     --inc SP
                     out_SP_in <= std_logic_vector( unsigned(out_SP_in) +1); 
                     empty_stack <= '0';
                 end if ;
+
+            --LDM
             elsif (OP = "10000") then 
                 Read_data <= Write_data;
+
+            --LDD
+            elsif (OP = "10010") then 
+                --Write_data: R[Rsrc1], Read_data: R[Rdst], Address: aluRes
+                --R[ Rdst ] ← M[R[ Rsrc] + offset]; 
+                Read_data <= data_memory(to_integer(unsigned(Write_data)+unsigned(Address)));
+            
+            --STD
+            elsif (OP = "10011") then 
+                --Write_data: R[Rsrc1], Address: aluRes, Rscr2: R[ Rsrc2]
+                --M[R[ Rsrc2] + offset] ←R[Rsrc1];  
+                data_memory(to_integer(unsigned(Rscr2)+unsigned(Address))) <= Write_data;
+            
             END IF;
 
         end if;
